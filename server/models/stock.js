d59ecff0821c4432c37386e6e08341a359aa0785
github username <github email address>
2019-09-28 21:12:48 -0400

    restructured resolvers

---
+++ server/models/stock.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const stockSchema = new Schema({
+  userId: {
+    type: String
+  },
+  ticker: {
+    type: String,
+    required: true
+  },
+  shares: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Stock', stockSchema)
